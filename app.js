a0c96fecf2969cfdb7fd8383d4fe8f9e9b5e155b
Kerim <kerimomerbegovic@hotmail.com>
2023-02-06 16:13:16 +0100

    Created function that shows objects from array

--- app.js
+++ app.js
@@ -8,9 +8,17 @@ function Book(bookName, author, pages, read) {
 }
 function addBooktoLibrary() {
   knjiga = new Book();
-  knjiga.bookName = prompt("b");
-  knjiga.author = prompt("a");
-  knjiga.pages = prompt("p");
-  knjiga.read = prompt("r");
+  knjiga.bookName = prompt('b');
+  knjiga.author = prompt('a');
+  knjiga.pages = prompt('p');
+  knjiga.read = prompt('r');
   myLibrary.push(knjiga);
+}
+
+function loop() {
+  myLibrary.forEach((book) => {
+    const div = document.createElement('div');
+    div.innerText = `name ${book.bookName} author ${book.author}`;
+    document.body.appendChild(div);
+  });
 }
